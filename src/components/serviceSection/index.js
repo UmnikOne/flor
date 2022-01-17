96496b1b314c37f76937e1ae73da8b183ff97c00
Roman <umnik.dev@gmail.com>
2022-01-17 14:55:08 +0300

    presentation

--- src/components/serviceSection/index.js
+++ src/components/serviceSection/index.js
@@ -11,48 +11,48 @@ const ServiceSection = () => {
         <Box id="home" sx={{ background: '#1f2623', color: '#fff', padding: { xs: '50px', md: '80px' }, pt: { xs: '50px', md: '150px' } }}>
             {/* <Typography className="app" sx={{ fontSize: { xs: '40px', md: '80px' }, fontWeight: '700', mb: '50px' }}>{t('services')}</Typography> */}
             <Zoom>
-            <Typography className="app" sx={{
-                fontSize: '20px',
-                fontStyle: 'italic',
-                color: '#a6296e',
-                textAlign: 'center',
-                maxWidth: { xs: '100%', md: '70%' },
-                margin: '0 auto 50px'
-            }}>{t('h1homeection')}</Typography>
-            <Grid container spacing={8}>
-                <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
-                    <Link href="/#whyKaymite" sx={{ color: '#fff', textDecoration: 'none' }}>
-                        <img width="100%" src={img1} />
-                        <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('PourquoiKaymite')}</Typography>
-                    </Link>
+                <Typography className="app" sx={{
+                    fontSize: '20px',
+                    fontStyle: 'italic',
+                    color: '#a6296e',
+                    textAlign: 'center',
+                    maxWidth: { xs: '100%', md: '70%' },
+                    margin: { xs: '100px auto 50px', md: '0 auto 50px' }
+                }}>{t('h1homeection')}</Typography>
+                <Grid container spacing={8}>
+                    <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
+                        <Link href="/#whyKaymite" sx={{ color: '#fff', textDecoration: 'none' }}>
+                            <img width="100%" src={img1} />
+                            <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('PourquoiKaymite')}</Typography>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
+                        <Link href="/#services" sx={{ color: '#fff', textDecoration: 'none' }}>
+                            <img width="100%" src={img2} />
+                            <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('ServicesSecteurs')}</Typography>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
+                        <Link href="/#ourSectors" sx={{ color: '#fff', textDecoration: 'none' }}>
+                            <img width="100%" src={img3} />
+                            <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('Notrevaleur')}</Typography>
+                        </Link>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
-                    <Link href="/#services" sx={{ color: '#fff', textDecoration: 'none' }}>
-                        <img width="100%" src={img2} />
-                        <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('ServicesSecteurs')}</Typography>
-                    </Link>
-                </Grid>
-                <Grid item xs={12} md={4} sx={{ cursor: 'pointer' }}>
-                    <Link href="/#ourSectors" sx={{ color: '#fff', textDecoration: 'none' }}>
-                        <img width="100%" src={img3} />
-                        <Typography className="app" sx={{ fontWeight: '700', fontSize: '18px', mt: '10px' }}>{t('Notrevaleur')}</Typography>
-                    </Link>
-                </Grid>
-            </Grid>
-            <Typography className="app" sx={{
-                fontSize: '18px',
-                fontWeight: '700',
-                color: '#fff',
-                textAlign: 'left',
-                margin: '50px auto 0px'
-            }}>{t('descriptionHomeSection3')}</Typography>
-            <Typography className="app" sx={{
-                fontSize: '18px',
-                fontStyle: 'italic',
-                color: '#a6296e',
-                textAlign: 'left',
-                margin: '0 auto 50px'
-            }}>{t('descriptionHomeSection4')}</Typography>
+                <Typography className="app" sx={{
+                    fontSize: '18px',
+                    fontWeight: '700',
+                    color: '#fff',
+                    textAlign: 'left',
+                    margin: '50px auto 0px'
+                }}>{t('descriptionHomeSection3')}</Typography>
+                <Typography className="app" sx={{
+                    fontSize: '18px',
+                    fontStyle: 'italic',
+                    color: '#a6296e',
+                    textAlign: 'left',
+                    margin: '0 auto 50px'
+                }}>{t('descriptionHomeSection4')}</Typography>
             </Zoom>
         </Box>
     )
